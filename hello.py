9c21fc866c29d35fdc6e73caf5990c7e99439280
Chris Ducastel <ducastel.chris@gmail.com>
2021-10-27 15:23:48 +0200

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,31 @@
+# $env:FLASK_ENV = "development"
+# $env:FLASK_APP = "hello"
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+# Create a route decorator
+@app.route('/')
+
+# def index():
+#     return "<h1>hello world</h1>"
+
+def index():
+    return render_template("index.html")
+
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html", username=name)
+
+# Create Custom Error Pages
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
+
